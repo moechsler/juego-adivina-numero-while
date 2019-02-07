4b4a739e6b0b57c76b921c4838bb0fad707d3179
Marco Oechsler <moechsler@gmail.com>
2019-02-06 23:30:29 -0300

    Agregar archivos de proyecto.

---
+++ juego adivina numero while/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace juego_adivina_numero_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random numero = new Random();
+
+            int aleatorio = numero.Next(0, 100);
+
+            int minumero = 101;
+
+
+         // Console.WriteLine($"el numero es {aleatorio}");
+        
+            int intentos = 0;
+
+            Console.WriteLine("Introduce un numero de 0 a 100");
+
+            while (aleatorio != minumero)
+            {
+
+                intentos++;
+
+                minumero = int.Parse(Console.ReadLine());
+
+                if (minumero > aleatorio) { Console.WriteLine("El numero es mas alto"); }
+
+                if (minumero < aleatorio) { Console.WriteLine("El numero es mas bajo"); }
+
+            }
+            Console.WriteLine($"has acertado, haz necesitado de {intentos} intentos.");
+            Console.ReadKey();
+        }
+
+            
+           
+        
+    }
+}
